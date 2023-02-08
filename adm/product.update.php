4f4aba1de5bea791767188feadcaed0c433c514c
born <borntobones@gmail.com>
2023-02-08 10:03:32 +0900

    fix: 회원가입 필드 변경

--- adm/product.update.php
+++ adm/product.update.php
@@ -6,11 +6,34 @@ include_once("../include/header.php");
 
     <section class="productupd">
         <div class="panel">
-            <label class="title">상품명</label><input type="text" placeholder="상품명을입력하세요" class="title" />
+            <label class="title">상품명</label><input type="text" placeholder="상품명을 입력하세요" class="title" />
             <button type="button" class="btn btn_red">등록하기</button>
         </div>
         <div class="box3">
-
+            <p class="name">기본 분류</p>
+            <p class="line"><label>카테고리</label><select><option>기획전</option><option>빈용방</option></select></p>
+            <p class="name">그룹 분류</p>
+            <p class="line"><span class="select"><input type="radio" id="select" name="group"/><label for="select"/>그룹</label>
+                            <input type="radio" id="select2" name="group"/><label for="select2">개별</label></span></p>
+            <p class="line">            <label>카테고리</label><select><option>기획전</option><option>빈용방</option></select></p>
+            <p class="name">가격 정보</p>
+            <p class="line"><label>상품명</label><input type="text" placeholder="상품명을 입력하세요"/><button type="button" class="btn_whiteline">옵션 추가</button></p>
+            <p class="name">옵션 정보</p>
+            <p class="line"><label>옵션명</label><input type="text" placeholder="옵션명을 입력하세요"/>
+                            <br class="visible-md2"/><label>추가 가격</label><input type="text" placeholder="추가 가격을 입력하세요"/>
+                            <button type="button" class="btn_whiteline"><i class="fa-solid fa-xmark"></i></button></p>
+            <p class="name">처방 정보</p>
+            <p class="line"><label>약재명</label><input type="text" placeholder="약재명을 입력하세요"/>
+                            <br class="visible-md2"/><label>원산지</label><input type="text" placeholder="원산지을 입력하세요"/>
+                            <br class="visible-md2"/><label>원산지</label><input type="text" placeholder="원산지를 입력하세요"/> <button type="button" class="btn_whiteline">구성 추가</button></p>
+            <p class="line"><label>약재명</label><input type="text" placeholder="약재명을 입력하세요"/>
+                            <br class="visible-md2"/><label>원산지</label><input type="text" placeholder="원산지을 입력하세요"/>
+                            <br class="visible-md2"/><label>원산지</label><input type="text" placeholder="원산지를 입력하세요"/>
+                            <button type="button" class="btn_whiteline"><i class="fa-solid fa-xmark"></i></button></p>
+            <p class="name">상품 정보</p>
+            <p class="line"><label>포장 방법</label><select class="st-sm"><option>환/ea</option><option>g/ea</option><option>개/상자</option><option>팩/ea</option></select>
+                            <input type="text" placeholder="0" class="ip-sm"/><label>환</label><input type="text" placeholder="0" class="ip-sm"/><label>ea</label>
+                            <br class="visible-md2"/><input type="text" placeholder="포장 방법을 입력하세요"/></p>
         </div>
     </section>
 
--- med/sign_up.php
+++ med/sign_up.php
@@ -14,47 +14,53 @@ include_once("../include/header.php");
 
         <form>
           <div class="sign_form">
-            <div class="form_wrap">
               <div>
-                <input type="text" placeholder="아이디"/>
-                <input type="password" placeholder="비밀번호"/>
-                <input type="password" placeholder="비밀번호 확인"/>
+                  <p>회원 정보</p>
+                    <div class="form_wrap">
+                      <div>
+                        <input type="text" placeholder="아이디"/>
+                        <input type="text" placeholder="성명"/>
+                      </div>
+                      <div>
+                        <input type="password" placeholder="비밀번호"/>
+                        <input type="password" placeholder="비밀번호 확인"/>
+                      </div>
+                      <div>
+                          <input type="date" data-placeholder="생년월일" required aria-required="true"/>
+                          <input type="text" placeholder="휴대폰번호"/>
+                      </div>
+                    </div>
               </div>
               <div>
-                <input type="email" placeholder="이메일"/>
-                <input type="text" placeholder="직장/한의원명"/>
-                <input type="text" placeholder="업태"/>
+                  <p>한의원 정보</p>
+                    <div class="form_wrap">
+                      <div>
+                          <input type="text" placeholder="한의원명"/>
+                          <input type="text" placeholder="사업자등록번호"/>
+                          <input type="text" placeholder="기본주소"/>
+                          <!--주소 필드 클릭시 우편번호 찾기-->
+                          <input type="text" placeholder="상세주소"/>
+                      </div>
+                      <div>
+                        <input type="text" placeholder="대표자명"/>
+                        <input type="text" placeholder="업태"/>
+                        <input type="text" placeholder="대표전화"/>
+                        <input type="text" placeholder="팩스번호"/>
+                      </div>
+                      <div>
+                        <input type="email" placeholder="이메일"/>
+                        <dl class="file_wrap">
+                          <dt>사업자등록증(면허증)</dt>
+                          <dd><a class="btn btn_black">파일첨부</a> 파일을 선택하세요..</dd>
+                        </dl>
+                        <dl class="file_wrap">
+                          <dt>원외탕전실 계약서</dt>
+                          <dd><a class="btn btn_black">파일첨부</a> 파일을 선택하세요..</dd>
+                        </dl>
+                        <a class="btn btn_large btn_red">계약서 파일 다운로드</a>
+                      </div>
+                    </div>
               </div>
-              <div>
-                <input type="text" placeholder="우편번호"/>
-                <input type="text" placeholder="사업자주소"/>
-                <!--우편번호/자업자주소 필드 클릭시 우편번호 찾기-->
-                <input type="text" placeholder="상세주소"/>
-              </div>
-            </div>
-            <div class="form_wrap">
-              <div>
-                <input type="text" placeholder="성명"/>
-                <input type="text" placeholder="휴대폰번호"/>
-                <input type="date" placeholder="생년월일"/>
-              </div>
-              <div>
-                <input type="text" placeholder="사업자등록번호"/>
-                <input type="text" placeholder="대표자명"/>
-                <input type="text" placeholder="대표전화"/>
-              </div>
-              <div>
-                <dl class="file_wrap">
-                  <dt>사업자등록증(면허증)</dt>
-                  <dd><a class="btn btn_black">파일첨부</a> 파일을 선택하세요..</dd>
-                </dl>
-                <dl class="file_wrap">
-                  <dt>원외탕전실 계약서</dt>
-                  <dd><a class="btn btn_black">파일첨부</a> 파일을 선택하세요..</dd>
-                </dl>
-                <a class="btn btn_large btn_red">계약서 파일 다운로드</a>
-              </div>
-            </div>
             <div class="btn_wrap">
               <a href="login.php" class="btn btn_sdw btn_red">회원가입요청</a>
               <a href="" class="btn btn_sdw btn_gray">최소하기</a>
