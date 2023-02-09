27aba02a78d0d7d60bfb6d7b52a59f508ddfe130
ZEN <whiteand5@gmail.com>
2023-02-09 13:27:51 +0900

    docs: 스크립트 삭제

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,9 @@
 
 $(document).ready(function(){
-  //체크박스디자인 div추가
+  /*/체크박스디자인 div추가
   $('input[type=checkbox] + label').prepend('<div></div>');
   $('input[type=radio] + label').prepend('<div></div>');
+  */
 
   //사이드바
   $(function(){
