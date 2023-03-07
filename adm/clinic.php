4bf303d6e49f374287152d857190eab2aee050e2
jay <nonono220@gmail.com>
2023-03-07 10:34:03 +0900

    docs: 한의원관리 필드 수정

--- adm/clinic.php
+++ adm/clinic.php
@@ -9,10 +9,11 @@ include_once("../include/header.php");
         <p>총 <span class="red">4</span>개 </p>
         <div>
             <select name="search">
-                <option value="">선택</option>
-                <option value="">전화번호</option>
-                <option value="">주소</option>
-                <option value="">사업자번호</option>
+                <option value="">한의원명</option>
+                <option value="">이름</option>
+                <option value="">아이디</option>
+                <option value="">대표자명</option>
+                <option value="">사업자등록번호</option>
             </select>
             <input class="search-bar" type="search" placeholder="검색어를 입력하세요" /><button type="button" class="btn_search"><i class="fa-light fa-magnifying-glass"></i></button>
 
@@ -21,11 +22,12 @@ include_once("../include/header.php");
     </div>
     <div class="tagbox">
         <div>
-            <p><strong>한의원명</strong></p>
+            <p><strong>그룹</strong></p>
         </div>
         <div>
             <p><a><span class="tag active">전체</span></a></p>
-            <p><a><span class="tag">전체</span></a></p>
+            <p><a><span class="tag">해밀</span></a></p>
+            <p><a><span class="tag">인애한의원</span></a></p>
         </div>
     </div>
     <div class="box3">
@@ -36,11 +38,13 @@ include_once("../include/header.php");
                     <th><input type="checkbox" name="checkbox"/></th>
                     <th>No.</th>
                     <th>한의원명</th>
-                    <th>대표자</th>
+                    <th>이름 /<br/>아이디</th>
+                    <th>대표자명</th>
                     <th>사업자등록번호</th>
                     <th>주소</th>
                     <th>대표전화</th>
-                    <th>직원 계정 수</th>
+                    <th>직원계정수</th>
+                    <th>그룹</th>
                     <th></th>
                 </tr>
                 </thead>
@@ -49,11 +53,13 @@ include_once("../include/header.php");
                     <td><input type="checkbox" name="checkbox"/></td>
                     <td>1</td>
                     <td>유신한의원</td>
+                    <td>김유신<br>test01</td>
                     <td>김유신</td>
                     <td>000-00-00000</td>
                     <td>부산시 해운대구 센텀동로 7</td>
                     <td>051-000-0000</td>
                     <td>4</td>
+                    <td>해밀</td>
                     <td><button type="button" class="btn btn_whiteline">수정</button><button type="button" class="btn btn_redline">삭제</button></td>
                 </tr>
                 </tbody>
